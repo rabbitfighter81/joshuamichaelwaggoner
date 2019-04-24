4f720c2099862d23fd8f662fa4df1f590e12541a
Joshua Michael <rabbitfighter@cryptolab.net>
2019-04-24 00:08:32 -0600

    Working on routing

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const port = process.env.PORT || 8000;
+
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(port);
-
-
-server.listen(port, function() {
-    console.log("App running on port " + port);
-})
-
-// PathLocationStrategy
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist', 'index.html'));
-});
+app.listen(process.env.PORT || 8080);
