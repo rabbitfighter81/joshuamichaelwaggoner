2f4a807ff8f6f6601ff92d212965025e6e349321
Joshua Michael <rabbitfighter@cryptolab.net>
2019-04-24 13:28:33 -0600

    Cleaning up server

--- server.js
+++ server.js
@@ -1,41 +1,45 @@
 const cors = require('cors');
 const path = require('path');
 const express = require('express');
-// const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const app = express();
-
-// const mail = require('./emailSenders');
 
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+// CORS
 app.use(cors());
 
+// More CORS settings
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
-// support json encoded bodies
+// Support json encoded bodies
 app.use(bodyParser.json());
-// support encoded bodies
+
+// Support encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set port
 app.set('port', process.env.PORT || 8080);
 
-app.get('/api', (req, res) => {});
-app.post('/api/sendEmail', (req, res) => {});
+// TODO: unused endpoints
+app.get('/api', (req, res) => { });
+app.post('/api/sendEmail', (req, res) => { });
 
+// Required for path location strategy
 app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Fire up the server and log it
 app.listen(app.get('port'), () => {
-    console.log(`Find the server at: http://localhost:${ app.get('port') }`); // eslint-disable-line no-console
+  console.log(`Find the server at: http://localhost:${ app.get('port') }`); // eslint-disable-line no-console
 });
