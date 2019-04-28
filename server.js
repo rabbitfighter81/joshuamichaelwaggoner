bfdaf8cf7050ce34c38b1abb37294b687e387b9d
Joshua Michael <rabbitfighter@cryptolab.net>
2019-04-28 02:03:48 -0600

    i18n

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.use("/en/", express.static(__dirname + "/dist/en"));
+app.use("/de/", express.static(__dirname + "/dist/de"));
 
 // CORS
 app.use(cors());
