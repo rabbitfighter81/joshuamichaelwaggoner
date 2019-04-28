476a5675500c900e37ed8ae4760ce406de3a85ca
Joshua Michael <rabbitfighter@cryptolab.net>
2019-04-28 07:41:00 -0600

    Snackbar-styles

--- src/app/components/snackbars/snackbar-coming-soon/snackbar-coming-soon.component.ts
+++ src/app/components/snackbars/snackbar-coming-soon/snackbar-coming-soon.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { MAT_SNACK_BAR_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-snackbar-coming-soon',
   templateUrl: './snackbar-coming-soon.component.html',
-  styleUrls: ['./snackbar-coming-soon.component.scss']
+  styleUrls: ['./snackbar-coming-soon.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SnackbarComingSoonComponent implements OnInit {
 
