c28baae605ef1241bf05ce7d1a0a3d7d83c86cfa
Joshua Michael <rabbitfighter@cryptolab.net>
2019-05-02 01:51:15 -0600

    Modularization and updates

--- locale-postbuild.sh
+++ locale-postbuild.sh
@@ -5,7 +5,7 @@
 echo $'\nInstalling and building locale bundles from source...'
 
 # Msg & Build for English
-echo $'---------------\n~~~ English ~~~\n---------------'
+echo $'\n---------------\n~~~ English ~~~\n---------------'
 ng build --prod --i18n-locale en --i18n-format xlf --i18n-file src/translate/messages.en.xlf --output-path=dist/en --baseHref /en/
 
 # Msg & Build for German
@@ -13,7 +13,7 @@ echo $'--------------\n~~~ German ~~~\n--------------'
 ng build --prod --i18n-locale de --i18n-format xlf --i18n-file src/translate/messages.de.xlf --output-path=dist/de --baseHref /de/
 
 # Msg & Build for Spanish
-echo $'---------------\n~~~ Spanish ~~~\n---------------'
+echo $'\n---------------\n~~~ Spanish ~~~\n---------------'
 ng build --prod --i18n-locale es --i18n-format xlf --i18n-file src/translate/messages.es.xlf --output-path=dist/es --baseHref /es/
 
 echo $'\n~~~ Fin! ~~~\n'
