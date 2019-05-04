c1bec760b40f6e182fc958f496f58c889c1f5b1c
Joshua Michael <rabbitfighter@cryptolab.net>
2019-05-04 14:30:45 -0600

    i18n stuff...

--- locale-postbuild.sh
+++ locale-postbuild.sh
@@ -6,14 +6,14 @@ echo $'\nInstalling and building locale bundles from source...'
 
 # Msg & Build for English
 echo $'\n---------------\n~~~ English ~~~\n---------------'
-ng build --prod -configuration --i18n-locale en --i18n-format xlf --i18n-file src/translate/messages.en.xlf --output-path=dist/en --baseHref /en/
+ng build --prod --i18n-locale en --i18n-format xlf --i18n-file src/translate/messages.en.xlf --output-path=dist/en --baseHref /en/
 
 # Msg & Build for German
 echo $'\n--------------\n~~~ German ~~~\n--------------'
-ng build --prod -configuration --i18n-locale de --i18n-format xlf --i18n-file src/translate/messages.de.xlf --output-path=dist/de --baseHref /de/
+ng build --prod --i18n-locale de --i18n-format xlf --i18n-file src/translate/messages.de.xlf --output-path=dist/de --baseHref /de/
 
 # Msg & Build for Spanish
 echo $'\n---------------\n~~~ Spanish ~~~\n---------------'
-ng build --prod -configuration --i18n-locale es --i18n-format xlf --i18n-file src/translate/messages.es.xlf --output-path=dist/es --baseHref /es/
+ng build --prod --i18n-locale es --i18n-format xlf --i18n-file src/translate/messages.es.xlf --output-path=dist/es --baseHref /es/
 
 echo $'\n~~~ Fin! ~~~\n'
