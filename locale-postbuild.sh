5cb5240b4ecc30e2fef7dce79544e0cd90fdc72c
Joshua Michael <rabbitfighter@cryptolab.net>
2019-05-05 23:58:30 -0600

    Brotoli settings

--- locale-postbuild.sh
+++ locale-postbuild.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # file: locale-postbuild.sh
 
--- server.js
+++ server.js
@@ -6,17 +6,21 @@ var app = express();
 var fs = require('fs');
 var brotli = require('brotli');
 
-
 var brotliSettings = {
   extension: 'br',
   skipLarger: true,
-  mode: 1, // 0 = generic, 1 = text, 2 = font (WOFF2)
-  quality: 10, // 0 - 11,
-  lgwin: 12 // default
+  mode: 0, // 0 = generic, 1 = text, 2 = font (WOFF2)
+  quality: 5, // 0 - 11,
+  lgwin: 12, // default
 };
 
 fs.readdirSync('dist/').forEach(function(file) {
-  if (file.endsWith('.js') || file.endsWith('.ts') || file.endsWith('.scss') || file.endsWith('.html')) {
+  if (
+    file.endsWith('.js') ||
+    file.endsWith('.ts') ||
+    file.endsWith('.scss') ||
+    file.endsWith('.html')
+  ) {
     var result = brotli.compress(fs.readFileSync('dist/' + file), brotliSettings);
     fs.writeFileSync('dist/' + file + '.br', result);
   }
@@ -24,22 +28,22 @@ fs.readdirSync('dist/').forEach(function(file) {
 
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.use("/en/", express.static(__dirname + "/dist/en"));
-app.use("/de/", express.static(__dirname + "/dist/de"));
-app.use("/es/", express.static(__dirname + "/dist/es"));
+app.use('/en/', express.static(__dirname + '/dist/en'));
+app.use('/de/', express.static(__dirname + '/dist/de'));
+app.use('/es/', express.static(__dirname + '/dist/es'));
 
 // CORS
 app.use(cors());
 
 // More CORS settings
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
 });
 
 // Support json encoded bodies
@@ -54,30 +58,26 @@ app.set('port', process.env.PORT || 8080);
 var greyhoundsFilePath = path.join(__dirname, 'src/db.greyhounds.json');
 
 app.get('/api/greyhounds', function(req, res) {
-    var readable = fs.createReadStream(greyhoundsFilePath);
-    readable.pipe(res);
+  var readable = fs.createReadStream(greyhoundsFilePath);
+  readable.pipe(res);
 });
-
-// Required for path location strategy
-/*
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-*/
 
 app.get('*', function(req, response) {
-    //this is for i18n
-    var supportedLocales = ['en', 'es', 'de'];
-    var defaultLocale = 'en';
-    var matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
-    //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
-    var locale = (matches && supportedLocales.indexOf(matches[1]) !== -1) ? matches[1] : defaultLocale;
+  //this is for i18n
+  var supportedLocales = ['en', 'es', 'de'];
+  var defaultLocale = 'en';
+  var matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
+  //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+  var locale =
+    matches && supportedLocales.indexOf(matches[1]) !== -1
+      ? matches[1]
+      : defaultLocale;
 
-    //res.render(`${ locale }/index`, { req });
-    response.sendFile(path.join(__dirname, 'dist/' + locale, 'index.html'));
+  //res.render(`${ locale }/index`, { req });
+  response.sendFile(path.join(__dirname, 'dist/' + locale, 'index.html'));
 });
 
 // Fire up the server and log it
 app.listen(app.get('port'), function() {
-    console.log('Find the server at: http://localhost:' + app.get('port')); // eslint-disable-line no-console
+  console.log('Find the server at: http://localhost:' + app.get('port')); // eslint-disable-line no-console
 });
