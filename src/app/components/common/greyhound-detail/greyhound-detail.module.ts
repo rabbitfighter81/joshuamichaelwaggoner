b539cad35b8390eb4909bc88fa148d42f449b817
Joshua Michael <rabbitfighter@cryptolab.net>
2019-05-08 12:02:12 -0600

    Organized imports and added pages for music and resources

--- src/app/components/common/greyhound-detail/greyhound-detail.module.ts
+++ src/app/components/common/greyhound-detail/greyhound-detail.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { GreyhoundService } from 'src/app/core/services/greyhound/greyhound.service';
+import { GreyhoundService } from '../../../core/services/greyhound/greyhound.service';
 import { SharedModule } from '../../../core/modules/shared/shared.module';
 import { TableGreyhoundModule } from '../../tables/table-greyhound/table-greyhound.module';
 import { GreyhoundDetailComponent } from './greyhound-detail.component';
