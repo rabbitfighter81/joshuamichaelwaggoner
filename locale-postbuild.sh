b00697c61ecd48f6a624b26a34b247647a825561
Joshua Michael <rabbitfighter@cryptolab.net>
2019-05-18 19:36:26 -0600

    Fix for 'From Yarn 1.0 onwards, scripts don't require -- for options to be forwarded. In a future version, any explicit -- will be forwarded as-is to the scripts'

--- locale-postbuild.sh
+++ locale-postbuild.sh
@@ -5,15 +5,15 @@ echo $'\nInstalling and building locale bundles from source...'
 
 # Msg & Build for English
 echo $'\n~~~~~~~~~~~~~~~\n~~~ English ~~~\n~~~~~~~~~~~~~~~\n'
-yarn run config -- --environment=prod && ng build --prod --i18n-locale en --i18n-format xlf --i18n-file src/translate/messages.en.xlf --output-path=dist/en --baseHref /en/
+yarn run config --environment=prod && ng build --prod --i18n-locale en --i18n-format xlf --i18n-file src/translate/messages.en.xlf --output-path=dist/en --baseHref /en/
 
 # Msg & Build for German
 echo $'\n~~~~~~~~~~~~~~\n~~~ German ~~~\n~~~~~~~~~~~~~~\n'
-yarn run config -- --environment=prod && ng build --prod  --i18n-locale de --i18n-format xlf --i18n-file src/translate/messages.de.xlf --output-path=dist/de --baseHref /de/
+yarn run config --environment=prod && ng build --prod  --i18n-locale de --i18n-format xlf --i18n-file src/translate/messages.de.xlf --output-path=dist/de --baseHref /de/
 
 # Msg & Build for Spanish
 echo $'\n~~~~~~~~~~~~~~~\n~~~ Spanish ~~~\n~~~~~~~~~~~~~~~\n'
-yarn run config -- --environment=prod && ng build --prod --i18n-locale es --i18n-format xlf --i18n-file src/translate/messages.es.xlf --output-path=dist/es --baseHref /es/
+yarn run config --environment=prod && ng build --prod --i18n-locale es --i18n-format xlf --i18n-file src/translate/messages.es.xlf --output-path=dist/es --baseHref /es/
 
 echo $'\nPlease update any missing translation files... :)\n'
 
